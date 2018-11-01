eefb25c09d638b4ec75656d3b675008ede0fb90c
Preston Sparks <psparks110@gmail.com>
2018-11-01 17:15:51 -0600

    Added File output with default folder on Desktop

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,27 +39,42 @@ namespace TimeClock
         DateTime breakStart;
         DateTime breakStop;
         TimeSpan elapsed = new TimeSpan(0);
+        List<string> lines = new List<string>();
 
         private void Clock_In_Click(object sender, RoutedEventArgs e)
         {
             if(isClockedIn)
             {
+                if(onBreak)
+                {
+                    onBreak = false;
+                    breakStop = DateTime.Now;
+                    elapsed += breakStop.Subtract(breakStart);
+                    lines.Add("\nEnd Break: " + breakStop.ToShortTimeString() + " : " + Note.Text);
+                    Log.Text += lines.Last();
+                    Break.Content = "Begin Break";
+                }
                 isClockedIn = false;
                 Break.IsEnabled = false;
                 TimeEnd.Text = DateTime.Now.ToShortTimeString();
                 stop = DateTime.Now;
                 ClockIn.Content = "Clock In";
-                Log.Text += "\nClocked Out: " + DateTime.Now + " : " + Note.Text;
+                lines.Add("\nClocked Out: " + DateTime.Now + " : " + Note.Text);
+                Log.Text += lines.Last();
                 Note.Text = "";
-                Log.Text += "\nElapsed Time: " + (stop.Subtract(start) - elapsed) + "\n";
+                lines.Add("\nElapsed Time: " + (stop.Subtract(start) - elapsed) + "\n");
+                Log.Text += lines.Last();
                 Log.ScrollToEnd();
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                File.WriteAllLines(@path + "\\Time Cards\\" + start.Year + "_" + start.Month + "_" + start.Day + "_" + start.Hour + "_" + start.Minute + "_" + start.Second + ".txt", lines);
             } else {
                 isClockedIn = true;
                 Break.IsEnabled = true;
                 TimeStart.Text = DateTime.Now.ToShortTimeString();
                 start = DateTime.Now;
                 ClockIn.Content = "Clock Out";
-                Log.Text += "Clocked In: " + DateTime.Now + " : " + Note.Text;
+                lines.Add("Clocked In: " + DateTime.Now + " : " + Note.Text);
+                Log.Text += lines.Last();
                 Log.ScrollToEnd();
                 Note.Text = "";
             }
@@ -77,13 +93,15 @@ namespace TimeClock
                 Break.Content = "Begin Break";
                 breakStop = DateTime.Now;
                 elapsed += breakStop.Subtract(breakStart);
-                Log.Text += "\nEnd Break: " + breakStop.ToShortTimeString() + " : " + Note.Text;
+                lines.Add("\nEnd Break: " + breakStop.ToShortTimeString() + " : " + Note.Text);
+                Log.Text += lines.Last();
                 Note.Text = "";
             } else {
                 onBreak = true;
                 Break.Content = "End Break";
                 breakStart = DateTime.Now;
-                Log.Text += "\nBegin Break: " + breakStart.ToShortTimeString() + " : " + Note.Text;
+                lines.Add("\nBegin Break: " + breakStart.ToShortTimeString() + " : " + Note.Text);
+                Log.Text += lines.Last();
                 Note.Text = "";
             }
         }
